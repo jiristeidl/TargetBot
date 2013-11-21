c9c94709b15f71e46651c8425fd401fa28e0ae41
jiristeidl <jiristeidl+github@gmail.com>
2013-11-21 01:46:45 +0100

    Starting to work on Logic

--- TargetBot/Default.cs
+++ TargetBot/Default.cs
@@ -1,19 +1,43 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace TargetBot
 {
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            int stories = 0;
+            int[] storyIds;
+            List<JToken> sortedStories;
+            JObject storiesRaw = JsonManipulator.createStories(TargetCommander.GetStories());
+            sortedStories = sortStories(storiesRaw);
             
-            JObject test = JObject.Parse(TargetCommander.GetTaskState(238));
-            Console.WriteLine(test);
+            
+
             
 
             
             
+        }
+        static List<JToken> sortStories(JObject stories)
+        {
+            List<JToken> storiesWithTasks = new List<JToken>();
+            foreach (var story in stories["Items"])
+            {
+                
+                //Console.WriteLine(story["Tasks"]["Items"]);
+                if (story["Tasks"]["Items"].ToString() != "[]")
+                {
+                    storiesWithTasks.Add(story);
+                }
+            }
+            return storiesWithTasks;
+        }
+        static int[] selectIds(List<JToken> list)
+        {
+            return new int[5];
         }
 
 
--- TargetBot/TargetCommander.cs
+++ TargetBot/TargetCommander.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TargetBot
 {
@@ -56,6 +54,9 @@ namespace TargetBot
                 return null;
             }            
 
+        }
+        public static void UpdateStoryState(JObject json)
+        {
         }
 
     }
