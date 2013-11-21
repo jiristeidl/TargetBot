61c7bd4a1149473a88a4ca346c72742c35655338
jiristeidl <jiristeidl+github@gmail.com>
2013-11-21 10:55:54 +0100

    added getBugState method

--- TargetBot/TargetCommander.cs
+++ TargetBot/TargetCommander.cs
@@ -55,6 +55,27 @@ namespace TargetBot
             }            
 
         }
+        public static string GetBugState(int id)
+        {
+            Console.WriteLine("Retrieving state for bug " + id);
+            WebRequest req = WebRequest.Create(baseUrl + "/api/v1/Tasks/" + id + "?include=[EntityState]&format=json");
+            req.Credentials = auth;
+            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+            if (resp.StatusCode == HttpStatusCode.OK)
+            {
+                using (Stream respStream = resp.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(respStream, Encoding.UTF8);
+                    Console.WriteLine("Success!");
+                    return reader.ReadToEnd();
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Format("Status Code: {0}, Status Description: {1}", resp.StatusCode, resp.StatusDescription));
+                return null;
+            }            
+        }
         public static void UpdateStoryState(JObject json, int id)
         {
             Console.WriteLine("Updating story " + id);
